153465bb248a896ff036fe2a69cc9d44dd055617
thungasaikumar <40800853+thungasaikumar@users.noreply.github.com>
2018-07-11 15:05:14 +0530

    Update 18.py

--- 18.py
+++ 18.py
@@ -1,1 +1,12 @@
-
+p,q=input().split()
+p=int(p)
+q=int(q)
+for i in range(p+1,q):
+    x=i
+    c=0
+    while(x!=0):
+        d=x%10
+        c=c+(d*d*d)
+        x=int(x/10)
+    if(c==i):    
+        print(i)
