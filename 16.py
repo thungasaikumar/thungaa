44a89a3282b5c32d5510894f785f20241bace69e
thungasaikumar <40800853+thungasaikumar@users.noreply.github.com>
2018-07-11 14:34:01 +0530

    Update 16.py

--- 16.py
+++ 16.py
@@ -3,7 +3,10 @@ p=int(p)
 q=int(q)
 for i in range(p+1,q):
     c=0
-    for j in range(1,int(i))
-        if(i%j==0)
-        c=c+1
+    for j in range(1,int(i)):
+        if(i%j==0):
+            c=c+1
+    if(c==1):
+        print(i,end=' ')
+        
         
