6be7df9a08108e4f3def87b5620a55530a1e4b2a
thungasaikumar <40800853+thungasaikumar@users.noreply.github.com>
2018-07-12 14:45:26 +0530

    Update 15.py

--- 15.py
+++ 15.py
@@ -1,6 +1,13 @@
 p,q=input().split()
 p=int(p)
 q=int(q)
+z=[]
 for i in range(p+1,q):
     if i%2==0:
-        print(i,end=' ')
+    	z.append(i)
+x=len(z)
+for i in range(0,x):
+    if(i==x-1):
+        print(z[i])
+    else:
+        print(z[i],end=' ')
