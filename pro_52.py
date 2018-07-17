1a8e9f24093483ede40892cff0a490a170b99f7d
thungasaikumar <40800853+thungasaikumar@users.noreply.github.com>
2018-07-17 14:18:56 +0530

    Create pro_52.py

---
+++ pro_52.py
@@ -0,0 +1,22 @@
+x=[0,0,0,0]
+y=[0,0,0,0]
+c=0
+x[0],y[0]=input().split()
+x[1],y[1]=input().split()
+x[2],y[2]=input().split()
+x[3],y[3]=input().split()
+for i in range(0,4):
+    for j in range(1,4):
+        if int(x[i])-int(x[j])==0:
+           x[i]=0
+           x[j]=0
+        if int(y[i])-int(y[j])==0:
+           y[i]=0
+           y[j]=0
+for i in range(0,4):
+    if(int(x[i]!=0)) or int(y[j]!=0):
+    	c=c+1
+if(c==0):
+    print("yes")
+else:
+    print("no")
