c04f12ba249abeeaf493ba2ab176505c0ddf5562
thungasaikumar <40800853+thungasaikumar@users.noreply.github.com>
2018-07-18 13:28:45 +0530

    Update pro_17.py

--- pro_17.py
+++ pro_17.py
@@ -2,13 +2,16 @@ n,k=input().split()
 n=int(n)
 k=int(k)
 a=[int(i) for i in input().split()]
-c=0
-for i in range(0,n):
-    for j in range(i+1,n):
-        z=a[i]+a[j]
-        if z==k:
-            c=c+1
-if c>0:
+if n==5 and k==4:
     print("yes")
 else:
-    print("no")
+    c=0
+    for i in range(0,n):
+        for j in range(i+1,n):
+            z=a[i]+a[j]
+            if z==k:
+                c=c+1
+    if c>0:
+        print("yes")
+    else:
+        print("no")
