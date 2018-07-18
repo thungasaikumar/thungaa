09a38814405afb9aa683979865ce1615a3987d9e
thungasaikumar <40800853+thungasaikumar@users.noreply.github.com>
2018-07-18 13:21:55 +0530

    Create pro_17.py

---
+++ pro_17.py
@@ -0,0 +1,14 @@
+n,k=input().split()
+n=int(n)
+k=int(k)
+a=[int(i) for i in input().split()]
+c=0
+for i in range(0,n):
+    for j in range(i+1,n):
+        z=a[i]+a[j]
+        if z==k:
+            c=c+1
+if c>0:
+    print("yes")
+else:
+    print("no")
